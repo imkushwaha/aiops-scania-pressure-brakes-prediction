8e99469b076bbabb2c4cf1a20b25ca2ff3b25162
Upendra <upendra.kumar48762@gmail.com>
2023-03-21 23:25:51 +0530

    init changed

--- init_setup.sh
+++ init_setup.sh
@@ -1,10 +1,10 @@
 echo [$(date)]: "START"
 echo [$(date)]: "Creating conda enviroment"
 
-conda create --prefix ./env python==3.8 -y
-echo [$(date)]: "Activating env viroment"
+conda create --prefix ./env python==3.10 -y
+echo [$(date)]: "Activating environment"
 
-source ~/miniconda3/etc/profile.d/conda.sh
+#source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ./env
 echo [$(date)]: "Installing dev requirements"
 
